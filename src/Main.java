9ca515ca903f89e869deee11b2badd872366640e
Abimoldayeva Tomiris <abimoldayevat@gmail.com>
2022-03-28 13:29:51 +0600

    added multiply method

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         System.out.println("Hello from Tomiris");
         printHelloWorldFromYerzhan();
         System.out.println(sumNum(5, 10));
+        System.out.println(multiple(2, 2));
 
     }
 
@@ -11,6 +12,9 @@ public class Main {
     }
     public static int sumNum(int a, int b){
         return a+b;
+    }
+    public static int multiple(int a, int b){
+        return a*b;
     }
 
 }
