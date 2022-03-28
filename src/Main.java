47041e10e0978628c416cb15f72f9a67197e59ad
nAzhre <erzh896@gmail.com>
2022-03-28 13:15:05 +0600

    add bubble sort function from Yerzhan

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,31 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from Tomiris");
-<<<<<<< HEAD
         printHelloWorldFromYerzhan();
+        System.out.println(sumNum(5, 10));
+
     }
 
     public static void printHelloWorldFromYerzhan(){
         System.out.println("Hello world from Yerzhan");
-=======
-        System.out.println(sumNum(5, 10));
->>>>>>> sum_nums
     }
     public static int sumNum(int a, int b){
         return a+b;
+    }
+
+    void bubbleSort(int arr[])
+    {
+        // with love from Yerzhan <3
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (arr[j] > arr[j+1])
+                {
+                    // swap arr[j+1] and arr[j]
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
     }
 
 }
