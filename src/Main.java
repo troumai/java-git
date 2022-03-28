046d92cbbd06202aaefc3a5a82202ea59d208e12
nAzhre <erzh896@gmail.com>
2022-03-28 13:17:30 +0600

    fix sum nums bag

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from Tomiris");
-<<<<<<< HEAD
         printHelloWorldFromYerzhan();
+        System.out.println(sumNum(5, 10));
+
     }
 
     public static void printHelloWorldFromYerzhan(){
         System.out.println("Hello world from Yerzhan");
-=======
-        System.out.println(sumNum(5, 10));
->>>>>>> sum_nums
     }
     public static int sumNum(int a, int b){
         return a+b;
