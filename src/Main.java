5cf91d842ba637861dfcc892e3b79f965cfc9f93
Abimoldayeva Tomiris <abimoldayevat@gmail.com>
2022-03-28 11:51:39 +0600

    sum_nums method added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from Tomiris");
+        System.out.println(sumNum(5, 10));
     }
+    public static int sumNum(int a, int b){
+        return a+b;
+    }
+
 }
