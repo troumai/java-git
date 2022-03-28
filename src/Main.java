387bd37de91fd79c87e648d7d9dda2fdc2e7e072
nAzhre <erzh896@gmail.com>
2022-03-28 11:58:27 +0600

    Add new hello world fucntion from Yerzhan

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from Tomiris");
+        printHelloWorldFromYerzhan();
+    }
+
+    public static void printHelloWorldFromYerzhan(){
+        System.out.println("Hello world from Yerzhan");
     }
 }
